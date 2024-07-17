296f6e962c3d39a4dd8eb9c8acb3fa1d9bf4b0b7
Matheo <garerimath73660@gmail.com>
2024-07-17 17:00:55 +0200

    Add cursor presence

--- components/room.tsx
+++ components/room.tsx
@@ -8,16 +8,21 @@ import {
 } from "@liveblocks/react/suspense";
 
 interface RoomProps {
-    children: ReactNode
-    roomId: string;
-    fallback: NonNullable<ReactNode> | null;
+  children: ReactNode
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
 }
 
 export function Room({ children, roomId, fallback }: RoomProps) {
   return (
-    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"} > 
-    {/* publicApiKey={"pk_dev_tD5TavX7rg9Lm7K7zfPbFhqfw97hwzER8kRvXG2PK8Fr5TvHqNKp8cuoV4EUSsrx"} */}
-      <RoomProvider id={roomId} initialPresence={{}}>
+    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"} throttle={16} >
+      {/* publicApiKey={"pk_dev_tD5TavX7rg9Lm7K7zfPbFhqfw97hwzER8kRvXG2PK8Fr5TvHqNKp8cuoV4EUSsrx"} */}
+      <RoomProvider
+        id={roomId}
+        initialPresence={{
+          cursor: null, //{ x: 0, y: 0 }
+        }}
+      >
         <ClientSideSuspense fallback={fallback}>
           {children}
         </ClientSideSuspense>
